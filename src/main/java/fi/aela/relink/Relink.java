e1cd399bc39f77521aad7ac78a2635f71005f833
AelaQt <me@aela.fi>
2022-06-12 05:42:44 +0300

    things

--- src/main/java/fi/aela/relink/Relink.java
+++ src/main/java/fi/aela/relink/Relink.java
@@ -1,10 +1,14 @@
 package fi.aela.relink;
 
 import net.fabricmc.api.ModInitializer;
+
+import javax.security.auth.login.LoginException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import fi.aela.relink.config.Config;
+import fi.aela.relink.discord.Bot;
 
 public class Relink implements ModInitializer {
 
@@ -17,5 +21,8 @@ public class Relink implements ModInitializer {
 		LOGGER.info(token);
 		String channel = Config.getChannel();
 		LOGGER.info(channel);
+		try {
+			Bot.build();
+		} catch (LoginException e) {}
 	}
 }
---
+++ src/main/java/fi/aela/relink/discord/Bot.java
@@ -0,0 +1,32 @@
+package fi.aela.relink.discord;
+
+import javax.security.auth.login.LoginException;
+
+import org.slf4j.Logger;
+
+import fi.aela.relink.Relink;
+import fi.aela.relink.config.Config;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+public class Bot extends ListenerAdapter {
+    Logger LOGGER = Relink.LOGGER;
+    public static void build() throws LoginException
+    {
+        JDABuilder.createLight(Config.getToken(), GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
+            .addEventListeners(new Bot())
+            .setActivity(Activity.playing("Minecraft"))
+            .build();
+    }
+    
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        String channel = event.getChannel().getId();
+        if (channel.equals(Config.getChannel())) {
+            LOGGER.info("[Discord] <" + event.getAuthor().getName() + "> " + event.getMessage().getContentRaw());
+        }
+    }
+}
